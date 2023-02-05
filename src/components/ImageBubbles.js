5689840e7ad3771ea309d4b1bcbe55345f21bf44
christophergayiuman <christophergayiuman@hotmail.com>
2023-02-05 16:10:55 +0000

    Upadted links to all buttons

--- src/components/ImageBubbles.js
+++ src/components/ImageBubbles.js
@@ -6,12 +6,20 @@ function onClick () {
     console.log("CLICK!!");
 }
 
+function ImageLink ({src, link}){
+    return(
+        <a target="blank" href={link}>
+            <img className="btn btn-ghost" src={src}/>
+        </a>
+    );
+}
+
 function ImageBubbles (){
     return(
         <div className="drop-shadow-lg flex space-x-11 justify-center h-10">
-            <img className="btn btn-ghost" src={linkedin}/>
-            <img className="btn btn-ghost" src={github} onClick={onClick}/>
-            <img className="btn btn-ghost" src={resume}/>
+            <ImageLink src={linkedin} link="https://www.linkedin.com/in/christophergayiuman/"/>
+            <ImageLink src={github} link="https://github.com/christophergyman"/>
+            <ImageLink src={resume} link=""/>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
-
 // Navigation Button component
-function NavBarButton({text, extra}){
-    return(
-        <button className={`btn text-bold normal-case ${extra}`}> {text} </button>
-    );
+function NavBarButton({ text, extra, link }) {
+  return (
+    <a href={link} target="blank">
+      <button className={`btn text-bold normal-case ${extra}`}> {text} </button>
+    </a>
+  );
 }
 // Navbar component
-function NavBar (){
-    return (
-         <div className="navbar p-5">
-            <div className="flex flex-1">
-                <NavBarButton extra={"btn-ghost text-xl"} text={"CHRISGYMAN.IO"}/>
-            </div>
-                <NavBarButton extra={"btn-ghost text-lg"} text={"Linkedin"}/>
-                <NavBarButton extra={"btn-accent text-lg"} text={"Resume"}/>
-        </div>
-    );
-}
+function NavBar() {
+  return (
+    <div className="navbar p-5">
+      <div className="flex flex-1">
+        <NavBarButton extra={"btn-ghost text-xl"} text={"CHRISGYMAN.IO"} />
+      </div>
+      <NavBarButton
+        extra={"btn-ghost text-lg"}
+        text={"Linkedin"}
+        link="https://www.linkedin.com/in/christophergayiuman/"
+      />
 
+      <NavBarButton extra={"btn-accent text-lg"} text={"Resume"} />
+    </div>
+  );
+}
 
 export default NavBar;
